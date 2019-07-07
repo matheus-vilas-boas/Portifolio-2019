254e6c3d6f57ff48376334f79e178f8ed6204c0c
MatheusVilas <mats.vilas@gmail.com>
2019-07-07 14:38:19 -0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+if (module.hot) {
+  module.hot.accept()
+}
+
+document.getElementById('spaceship').addEventListener('click', (evt) => {
+
+  if(evt.currentTarget.classList.contains('flying')) {
+    document.getElementById('header').classList.add('away');
+    evt.currentTarget.classList.remove('flying');
+    evt.currentTarget.classList.add('arrive');
+    return;
+  }
+  evt.currentTarget.classList.add('flying');
+})
