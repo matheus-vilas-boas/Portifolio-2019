0b31513d6985a7965426dc7a0bbf6b4758ecd09a
MatheusVilas <mats.vilas@gmail.com>
2019-07-14 00:46:02 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,11 @@ if (module.hot) {
 
 document.getElementById('spaceship').addEventListener('click', (evt) => {
 
-  if(evt.currentTarget.classList.contains('flying')) {
-    document.getElementById('header').classList.add('away');
-    evt.currentTarget.classList.remove('flying');
-    evt.currentTarget.classList.add('arrive');
-    return;
-  }
-  evt.currentTarget.classList.add('flying');
+  // if(evt.currentTarget.classList.contains('flying')) {
+  //   document.getElementById('header').classList.add('away');
+  //   evt.currentTarget.classList.remove('flying');
+  //   evt.currentTarget.classList.add('arrive');
+  //   return;
+  // }
+  // evt.currentTarget.classList.add('flying');
 })
